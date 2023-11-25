5d34c6cf1bdd33ab305b53bf7446ae967a34588a
eMode18 <ewagandi@gmail.com>
2023-11-25 12:24:52 -0800

    places

--- models/place.py
+++ models/place.py
@@ -35,20 +35,21 @@ class Place(BaseModel, Base):
     """
 
     __tablename__ = "places"
-    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
-    amenity_ids = []
+
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
     description = Column(String(1024))
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    max_guest = Column(Integer, default=0)
-    name = Column(String(128), nullable=False)
-    number_bathrooms = Column(Integer, default=0)
-    number_rooms = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
     reviews = relationship("Review", backref="place", cascade="delete")
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False)
+    amenity_ids = []
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
         def reviews(self):
